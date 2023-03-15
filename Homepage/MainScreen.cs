d5ba2f33b4ec19578f8084c5acb49c2acb212a54
isha.gupta2 <isha.gupta2@DEL1-LHP-N70107>
2023-03-15 09:37:39 +0530

    Add project files.

---
+++ Homepage/MainScreen.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homepage
+{
+    public class MainScreen
+    {
+            /*Admin.AdminLogin al = new Admin.AdminLogin();*/
+            /*admin al = new admin();
+            User user = new User();*/
+
+            public void mainscreen()
+            {
+
+                while (true)
+                {
+
+                    Console.WriteLine("1.> Admin ");
+                    Console.WriteLine("-----------------------");
+                    Console.WriteLine("2.> User  ");
+                    Console.WriteLine("-----------------------");
+                    Console.WriteLine("3.> Resister Account  ");
+                    Console.WriteLine("-----------------------");
+
+                    int n = int.Parse(Console.ReadLine());
+
+                    switch (n)
+                    {
+                        case 1:
+                            al.admin_login();
+                            break;
+                        case 2:
+                            user.userlogin();
+
+                            break;
+                        case 3:
+                            user.resisternewaccount();
+                            break;
+                    }
+                }
+            }
+        }
+    
+}
---
+++ TradingProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using Homepage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradingProject
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            MainScreen ms = new MainScreen();
+            ms.mainscreen();
+        }
+    }
+}
